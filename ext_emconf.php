36f6a9034fca0f62b621f7a2ad382e4df057f549
rovitch <sebastien-github@martinovitch.net>
2026-04-12 19:32:17 +0200

    [TASK] Update ext_emconf.php description to match github description

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Page Password',
-    'description' => 'PagePassword provides a simple way to restrict access to specific pages and their sub-pages with password authentication.',
+    'description' => 'PagePassword provides a simple way to restrict access to specific pages and their sub-pages with password authentication. This extension allows you to create password-protected sections or entire page trees within your TYPO3 website. Perfect for creating member areas, work-in-progress sections, pre-production environments, or any content that requires basic access control at the page level.',
     'state' => 'stable',
     'author' => 'rovitch',
     'author_email' => '',
